841c0ef1a129ff0c8fe4b777e458ed7b91443fe8
JerogZurc <jorgecruzprof@gmail.com>
2022-11-16 03:09:56 -0600

    carga de ejercicios 24, 25 y 26

--- ejercicio24.js
+++ ejercicio24.js
@@ -1,3 +1,24 @@
 /* Programa una función que dado un arreglo de números devuelva un objeto con dos arreglos, el primero tendrá los numeros ordenados en forma ascendente y el segundo de forma descendiente
-* Ejemplo: miFuncion(7,5,7,8,6) devuelve {asc: [5,6,7,7,8], desc:[8,7,7,6,5]}
+* Ejemplo: miFuncion([7,5,7,8,6]) devuelve {asc: [5,6,7,7,8], desc:[8,7,7,6,5]}
 */
+
+const ascDes = (arreglo = undefined) => {
+    if (arreglo === undefined) return console.warn('No se ingresó ningun arreglo');
+    if (!(arreglo instanceof Array)) return console.error("El valor que se ingreso no es un arreglo");
+    if (arreglo.length === 0) return console.error("El arreglo no contine ningun numero");
+
+    for (let num of arreglo) {
+        if (typeof num !== "number") return console.error("Los elementos del arreglo no son del tipo numérico");
+    }
+
+    return console.info({
+        asc: arreglo.map(el => el).sort(),
+        desc: arreglo.map(el => el).sort().reverse()
+    })
+}
+
+ascDes();
+ascDes({'nombre': "Jorge", 'apellido': "Cruz"});
+ascDes([]);
+ascDes(["1", 2, "3"]);
+ascDes([7,5,7,8,6]);
--- ejercicio25.js
+++ ejercicio25.js
@@ -1,3 +1,23 @@
 /* Programa una función que dado un arreglo de elementos, elimine los duplicados
 * Ejemplo: miFuncion(["x", 10, "x", 2, "10", 10, true, true]) devuelve ["x", 10, 2, "10", true].
 */
+
+const duplicado = (arreglo = undefined) => {
+    if (arreglo === undefined) return console.warn('No se ingresó ningun arreglo');
+    if (!(arreglo instanceof Array)) return console.error("El valor que se ingreso no es un arreglo");
+    if (arreglo.length === 0) return console.error("El arreglo no contine ningun numero");
+    if (arreglo.length === 1) return console.error("El arreglo debe contener al menos 2 elementos");
+
+    return console.info({
+        original:arreglo,
+        sinDuplicados: arreglo.filter((value, index, self) => self.indexOf(value) === index)
+    })
+
+
+}
+
+duplicado();
+duplicado({'nombre': "Jorge", 'apellido': "Cruz"});
+duplicado([]);
+duplicado(["1", 2, "3"]);
+duplicado(["x", 10, "x", 2, "10", 10, true, true]);
